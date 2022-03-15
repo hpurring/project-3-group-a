9678faa40c100a719a086b9eb90b4ea641ed7b6b
sihayah <91216737+sihayah@users.noreply.github.com>
2022-03-15 18:31:00 -0400

    changed buttons to Links

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { QUERY_ME } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
 import Auth from '../../utils/auth';
 import me from '../../MOCK_ME.json';
 
-const ProfilePage = () => {
+const Profile = () => {
 
 // const { data } = useQuery(QUERY_ME);
 
   if (!me.cards) {
       return(
-          <button>
-              Create my QRad
-          </button>
+          <>
+            <Link to='/editform'>
+              Create my QRad 
+            </Link>
+              
+          </>
       )
   }
 
   return(
-      <button>
-        Update my QRad
-      </button>
+    <Link to='/editform'>
+      Update my QRad 
+    </Link>
   )    
 }
 
-export default ProfilePage;
+export default Profile;
